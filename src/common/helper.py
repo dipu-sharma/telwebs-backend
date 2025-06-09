3a601709ac15521b88dca7fae35b84464c42faf7
Dipu Sharma <sdipu231@gmail.com>
2025-06-09 11:48:36 +0530

    commit

--- src/common/helper.py
+++ src/common/helper.py
@@ -2,14 +2,12 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
-# Constants (ideally loaded from environment variables)
 JWT_SECRET = "Sv/w?/T@^CN8RR$O8^I7Tss6'j76iyb"
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRE_MINUTES = 120
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Utils
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
